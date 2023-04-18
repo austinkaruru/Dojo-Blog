fcda8ec143384836368dcafb5d014fd8dd0fc383
Austin Mwangi <austin.mwangi@atlancis.com>
2023-04-18 12:21:29 +0300

    second commit

--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@ const Home = () => {
     // let name = 'mario';
     const [blogs,setBlogs] = useState([    
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Welcome partyyy!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
