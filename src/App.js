a9bf920e4eda1e38e508c938c4fd04f35aa5c288
Austin Mwangi <austin.mwangi@atlancis.com>
2023-04-20 10:36:33 +0300

    added add & delete blog and 404 page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import Navbar from './navbar';
 import Home from './home';
+import Create from './Create';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
  
   return (
+    <Router>
     <div className="App">
     <Navbar />
     <div className="content">
-     <Home />
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route path='/create' element={<Create/>} />
+        <Route path='/blogs/:id' element={<BlogDetails/>} />
+        <Route path='*' element={<NotFound />} />
+
+      </Routes>   
+       
     </div>
     </div>
+    </Router>
+   
   );
 }
 
--- src/home.js
+++ src/home.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import BlogList from './BlogList';
 import useFetch from './useFetch';
 const Home = () => {
@@ -6,9 +5,9 @@ const Home = () => {
     
     return (
         <div className="home">
-        { error && <div>{ error} </div>}
-            { isPending && <div>Loading....</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}    
+        { error && <div> { error } </div>}
+            { isPending && <div>Loading....</div> }
+            { blogs && <BlogList blogs= { blogs } title="All Blogs!" />}    
             
         </div>
       );
